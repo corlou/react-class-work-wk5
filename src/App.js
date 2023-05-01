a4dd8acdf8143459e26a0887e87f6be5631e3d05
Corina <corina@danitt.com>
2023-05-01 18:07:45 +0200

    JSX Components with img and video

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import SomeExampleComponent from './SomeExampleComponent';
+import BlackCatImg from './assets/BlackCatImg.jpg';
+import BlackCatVideo from './assets/BlackCatVideo.mp4'
+
+
+const someMessage = "Hello World!"
+
+const classFavouritePokemon = [
+  "Bulbasaur",
+  "Chikorita",
+  "the water one",
+  "Pikachu",
+  "Squirtle",
+  "Mankey",
+  "Voltorb",
+  "Poliwag",
+  "Geodude",
+  "Komala",
+  "Snorlax",
+  "Gumblesnorf"
+]
 
 function App() {
   return (
     <div className="App">
+      <SomeExampleComponent />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{someMessage}</h1>
+        <img src={BlackCatImg} alt="a black cat" width={300} height={200}></img>
+        <video src={BlackCatVideo} autoplay={true} controls width={300} height={400}>
+          Video not loading...
+        </video>
+        <h1>Class Pokemon</h1>
+        <ul>
+          {classFavouritePokemon.map((pokemon, index) => (
+            <li className="pokemon" key={index}>
+              {pokemon}
+            </li>
+          ))}
+        </ul>
       </header>
     </div>
   );
---
+++ src/SomeExampleComponent.js
@@ -0,0 +1,5 @@
+export default function SomeExampleComponent() {
+  return (
+    <h3>Some lil header</h3>
+  )
+}
